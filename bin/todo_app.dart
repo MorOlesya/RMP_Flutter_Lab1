0d80fc39cd66dcbbf813bdb3846d272a0ac04d34
MorOlesya <dl9morozova@gmail.com>
2026-04-08 14:20:26 +0300

    add todo

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,3 +1,17 @@
 void main() {
   print("Hello World");
+  String name = "Artem";
+  String? name2 = null;
+  var count = 0;
+  var title = "Hello";
+  var x = 6;
+  //x = "dsf";
+  final id;
+  id = 1;
+  const app = "todo";
+  print("$app, $name, $name2, ${count + 6}");
+  List<String> test = ["язык", "уроки"];
+  Map<String, dynamic> data = {"Key": 13};
+  Set<int> id2 = {1, 2, 3};
+  
 }
